a1d6cf1cea8cd315468e897f9d00ddf4a378841b
israel0909 <israelsbook@gmail.com>
2023-02-17 16:26:36 -0800

    print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main- I'll declare a var (of type char; cause of single character values
+ * then i'll assign letters to the var, and then putchar the var
+ *Return: 0
+ */
+
+int main(void)
+{
+	char letters[26] = "abcdefghijklmnopqrstuvwxyz";
+	int input;
+
+	for (input = 0; input <= 26; input++)
+	{
+		putchar(letters[input]);
+	}
+	putchar("\n"); /* I'm insert "\n" here and not above*/
+	/*; cause the task asks to use "putchar" twice*/
+	return (0);
+}
