ac6053b944811d3d1bd435196933563ace1f0d33
israel0909 <israelsbook@gmail.com>
2023-02-17 19:56:21 -0800

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main- going to be using greater than and less thans
+ * Return: 0
+*/
+
+int main(void)
+{
+	int first = 0;
+	int second = 0;
+
+	while (second < 10)
+	{
+		first = 0;
+		while (first < 10)
+		{
+			if (first != second && first > second)
+			{
+				putchar((first % 10) + '0');
+				putchar((second % 10) + '0');
+				putchar(',');
+				putchar(' ');
+			}
+			first++;
+		}
+		second++;
+	}
+	putchar('\n');
+	return (0);
+}
