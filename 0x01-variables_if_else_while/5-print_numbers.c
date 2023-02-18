03f50acf9e8407142736ffdc2b3d6a0868a47309
israel0909 <israelsbook@gmail.com>
2023-02-17 17:56:44 -0800

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main- declaring var,then printing digits
+ * Return: 0
+*/
+
+int main(void)
+{
+	int numb;
+
+	for (numb = 0; numb <= 9; numb++)
+	{
+		printf("%d\n", numb);
+	}
+	return (0);
+}
