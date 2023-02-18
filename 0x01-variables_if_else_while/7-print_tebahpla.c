1e7bf97198704bc2b33a9c688921c2514de25030
israel0909 <israelsbook@gmail.com>
2023-02-17 18:26:56 -0800

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main- going to reverse with increment relational operator
+ * Return: 0
+*/
+
+int main(void)
+{
+	char za;
+
+	for (za = 'z'; za => a; za--)
+	{
+		putchar (za);
+	}
+	putchar ('\n');
+	return (0);
+}
