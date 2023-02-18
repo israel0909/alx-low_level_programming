5e197da6021ae54eb7ca335817b9650a29119036
israel0909 <israelsbook@gmail.com>
2023-02-17 19:13:42 -0800

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,10 +11,13 @@ int main(void)
 
 	for (numb = 0; numb < 9; numb++)
 	{
-		putchar(numb % 10);
+		putchar((numb % 10) + '0');
+		if (numb != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
-	putchar(+ '0');
-	putchar(',');
-	putchar(' ');
+	putchar('\n');
 	return (0);
 }
