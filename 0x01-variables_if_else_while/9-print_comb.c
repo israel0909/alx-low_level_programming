39fb466920326a890f3aa42c2edc526d5782c8c3
israel0909 <israelsbook@gmail.com>
2023-02-17 18:54:48 -0800

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main- i'll will insert /n in the output
+ * Return: 0
+*/
+
+int main(void)
+{
+	int numb;
+
+	for (numb = 0; numb < 9; numb++)
+	{
+		putchar(numb + '\n');
+	}
+	return (0);
+}
