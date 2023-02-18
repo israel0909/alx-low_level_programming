a01816b640660596bf0b2d0e0e4ee9b7ffbb38c9
israel0909 <israelsbook@gmail.com>
2023-02-17 21:10:52 -0800

    task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+
+/**
+ * main- going to be using greater than and less thans
+ * Return: 0
+*/
+
+int main(void)
+{
+	int first;
+	int fst1;
+	int fst2;
+
+	int second;
+	int snd1;
+	int snd2;
+
+	while (first < 98)
+	{
+		fst1 = (first / 10 + '0');
+		fst2 = (first % 10 + '0');
+
+		second = 0;
+		while (second < 99)
+		{
+			snd1 = (second / 10 + '0');
+			f_d2 = (second % 10 + '0');
+			if (first < second)
+			{
+				putchar(fst1);
+				putchar(fst2);
+				putchar(' ');
+				putchar(snd1);
+				putchar(snd2);
+
+				if (first != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			second++;
+		}
+		first++;
+	}
+	putchar('\n');
+	return (0);
+}
