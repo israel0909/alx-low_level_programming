13583161de9bb0aae8fc36603f5da7ec0a0b7102
israel0909 <israelsbook@gmail.com>
2023-02-22 03:22:55 -0800

    testing task #10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add- addition of 2 int
+ * Return: addition of inputs from main.c
+ * @n: first input
+ * @o: second input
+*/
+
+int add(int n, int o)
+{
+	return (int n + int o);
+}
