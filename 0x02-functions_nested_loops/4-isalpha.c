26e8cbddd45ce848c43903f8f7b981933a200c70
israel0909 <israelsbook@gmail.com>
2023-02-21 19:27:32 -0800

    testing 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha- Entry
+ * @c: input
+ * Return: 1 if true, 0 otherwise
+ * output- gives 1 if out is true, 0 otherwise
+*/
+
+int _isalpha(int c)
+{
+
+	if ((c >= 97 && c <= 122) || (c >= 65 && <= 90))
+	{
+		return (1);
+	}
+	return (0);
+}
