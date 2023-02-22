f7a09cb5494182698b8dede14df2dac72676169a
israel0909 <israelsbook@gmail.com>
2023-02-21 19:54:12 -0800

    testing 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -16,17 +16,16 @@ int print_sign(int n)
 		return (1);
 	}
 
-	if (n == 0)
+	else if (n == 0)
 	{
 		_putchar(48);
 		return (0);
 	}
 
-	if (n < 0)
+	else if (n < 0)
 	{
 		_putchar('-');
 		return (-1);
 	}
 
 }
-
