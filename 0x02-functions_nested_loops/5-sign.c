97158b9b4fe637dd6d779c584a75c645008452a8
israel0909 <israelsbook@gmail.com>
2023-02-21 19:47:58 -0800

    testing 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_sign- Entry
+ * @n: input
+ * Return: 1 if positive, 0if zero, -1 if negative
+ * output
+*/
+
+int print_sign(int n)
+{
+
+	if (c > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+
+	if (c == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+
+	if (c < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+
+}
+
