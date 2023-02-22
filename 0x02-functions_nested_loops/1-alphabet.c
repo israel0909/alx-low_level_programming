e9d7a2fa813b975efccf379c958050093145c131
israel0909 <israelsbook@gmail.com>
2023-02-21 18:23:42 -0800

    testing file 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main- Ignition
+ * Return: 0
+ * output- letters of the alphabet using char
+*/
+
+int main(void)
+{
+	char x;
+
+	for (x = 97, x <= 122, x++)
+	{
+		_putchar(x);
+	}
+
+	_putchar('\n');
+	return (0);
+}
