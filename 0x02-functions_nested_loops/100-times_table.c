250ca8bb39aafed0c5a765884d0ee4c1f5f4f998
israel0909 <israelsbook@gmail.com>
2023-02-22 06:00:20 -0800

    tested #12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/** 
+ * print_times_table- outinf a multiplication table by input
+ * Return: multiplication table
+ * @n: input
+ * @c: column
+ * @r: row
+ * @k: product;r * c
+ */
+
+void print_times_table(int n)
+{
+	int c, r, k;
+
+	if (n >= 0 && n <= 14)
+	{
+		for (c = 0; c <= n; c++)
+		{
+			for (r = 0; r <= n; r++)
+			{
+				k = c * r;
+				if (k > 99)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+				else if (k > 9)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar(32);
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+				else
+				{
+					if (r != 0)
+					{
+						_putchar(',');
+						_putchar(32);
+						_putchar(32);
+						_putchar(32);
+					}
+					_putchar(k + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
