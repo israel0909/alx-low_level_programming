958bc4b388418cc41c431a88752992375fdfe551
israel0909 <israelsbook@gmail.com>
2023-02-23 06:37:19 -0800

    #2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * hmm getting here, i realized i skipped comment for task #1, nevertheless; i MOVE
+ * mul- multiplies 2 ints
+ * @a: 1st input
+ * @b: 2nd input
+ * Return: i'll get back to this...i think...
+*/
+
+int mul(int a, int b)
+{
+	int d;
+
+	d = a * b;
+	return (d);
+}
