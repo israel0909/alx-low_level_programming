00b7e6ddd1aa58da63fdedac75a7252e73ab50b4
israel0909 <israelsbook@gmail.com>
2023-02-27 11:13:33 -0800

    #3 4&5

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,13 +2,13 @@
 
 /**
  * _puts - prints a string output
- *
+ * @str: var
  * Return: nil
  */
 
 void _puts(char *str)
 {
-	for ( ; *str != '\0'; *str++)
+	for ( ; *str != '\0'; str++)
 	{
 		_putchar(*str);
 	}
