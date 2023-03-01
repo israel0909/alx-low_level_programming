434da989f8b7a5007c8550977c10ce2c4513df13
israel0909 <israelsbook@gmail.com>
2023-03-01 03:24:14 -0800

    #0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcat - concatinates two strings together.
+ * @*dest: 1st input
+ * @*src : 2nd input
+ * Return: Always 0.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	*dest;
+	*src;
+
+	return (strcat(dest, src));
+}
