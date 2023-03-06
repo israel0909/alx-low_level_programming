df78ea36ecd96a171357212f279fca9aa495455f
x9x96 <israelsbook@gmail.com>
2023-03-06 07:54:40 -0800

    test #0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - swaps d bytes of "*s 4 "b
+ * @*s: bytes to be swapped
+ * @b: bytes used to swap
+ * @n: determines number of bytes to swap
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	/*var 4 count*/
+	int x = 0;
+	/*lup tru "s, while < "n*/
+	for ( ; x < n; x++)
+	{
+		/*swap "s 4 "b*/
+		s[x] = b;
+	}
+	/*return "s*/
+	return (s);
+}
