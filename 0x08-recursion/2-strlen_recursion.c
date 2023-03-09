b2e18360a81a72e37a8e046de3b47fab40cace82
x9x96 <israelsbook@gmail.com>
2023-03-08 16:27:33 -0800

    fixing bugsssss

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,15 +8,13 @@
 
 int _strlen_recursion(char *s)
 {
-	/*exit; == '0*/
-	if (*s == '\0')
+	int len = 0;
+
+	if (*s)
 	{
-		return (0);
+		len++;
+		len += _strlen_recursion(s + 1);
 	}
-	/*if != exit*/
-	if (*s != '\0')
-	{
-		/*return 1 + "s arry increment*/
-		return (1 + _strlen_recursion(s + 1));
-	}
+
+	return (len);
 }
--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,71 +1,34 @@
 #include "main.h"
+#include <stdio.h>
 
-int find_sqrt(int num, int root);
-
-/**
- * square_checker - iterativly checks for square
- * @n: input
- * @x: squared value
- * Return: checked square
- */
-
-int square_checker(int x, int n)
-{
-	int y;
-
-	y = x * x;
-
-	if (y == n)
-	{
-		return (x);
-	}
-	else if (y < n)
-	{
-
-		return (_square_checker(x + 1, n));
-	}
-	else if (y > n)
-	{
-		return (-1);
-	}
-}
+int _sqrt(int n, int i);
 
 /**
- * _sqrt_recursion - returns the sqrt of n
- * @n: input
- * Return: sqrt n OR  -1 unsqrt-able
- */
-
-
+  * _sqrt_recursion - Returns the natural square root of a number
+  * @n: input
+  *
+  * Return: the natural square root
+  */
 int _sqrt_recursion(int n)
 {
-	int root = 0;
-
-	if (n < 0)
-		return (-1);
-
-	if (n == 1)
-		return (1);
-
-	return (find_sqrt(n, root));
+	return (_sq(n, 1));
 }
+
 /**
- * find_sqrt - Finds the natural square root of an inputted number.
- * @num: The number to find the square root of.
- * @root: The root to be tested.
- *
- * Return: If the number has a natural square root - the square root.
- *         If the number does not have a natural square root - -1.
- */
-
-int find_sqrt(int num, int root)
+  * _sq - Calculates natural square root
+  * @n: number to calculate the square root
+  * @i: iterator
+  *
+  * Return: the natural square root
+  */
+int _sq(int n, int i)
 {
-	if ((root * root) == num)
-		return (root);
+	int sq = i * i;
 
-	if (root == num / 2)
+	if (sq > n)
 		return (-1);
 
-	return (find_sqrt(num, root + 1));
+	if (sq == n)
+		return (i);
+	return (_sq(n, i + 1));
 }
-
