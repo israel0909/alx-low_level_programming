c91094c0de782af869c34a6a463c92a70adbc837
x9x96 <israelsbook@gmail.com>
2023-03-10 06:16:15 -0800

    test #0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - a program to output it's own name even when changed
+ * Return: 0
+ */
+
+int main(int argc, char** argv)
+{
+	printf("%s", argv[0]);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Intro to argv and argc
