8eafcaba5f0a5d4358058c561844353424a379c8
x9x96 <israelsbook@gmail.com>
2023-03-10 11:19:43 -0800

    fixing #0

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-#include <ctype.h>
 
 /**
  * _isdigit - checks if an input is in upper case and returns 1, else 0
@@ -10,18 +9,5 @@
 
 int _isdigit(int c)
 {
-
-	int d;
-
-	d = isdigit(c);
-
-	if (d != 0)
-	{
-		return (1);
-	}
-	else
-	{
-		return (d);
-	}
 }
 
