6cb5f55d1cf913a802954c49866319d8a0588a21
x9x96 <israelsbook@gmail.com>
2023-03-10 06:31:40 -0800

    test #0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -7,9 +8,9 @@
  * Return: 0
  */
 
-int main(int argc, char **argv)
+int main(int argc __attribute((unused)), char *argv[])
 {
-	void(argc);
-	printf("%s/n", argv[0]);
+	printf("%s\n", argv[0]);
+
 	return (0);
 }
