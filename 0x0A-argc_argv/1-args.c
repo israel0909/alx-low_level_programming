65224a30074b3a4d27cf2c01d9f85aa6a90fade7
x9x96 <israelsbook@gmail.com>
2023-03-10 06:51:23 -0800

    test#1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,10 +3,13 @@
 /**
  * main - oupputs number of arguments passed ino it
  * @argc: counts arguments
+ * @argv: outputs arguments
  * Return: 0
  */
 
-int main(int argc)
+int main(int argc, char *argv[])
 {
+	(void) argv;
 	printf("%d\n", argc - 1);
+	return (0);
 }
