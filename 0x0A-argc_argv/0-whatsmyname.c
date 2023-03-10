6d0cd7eafc9d1248ccd34b2daa905017f058ea3e
x9x96 <israelsbook@gmail.com>
2023-03-10 06:39:39 -0800

    test #0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,16 +1,17 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - a program to output it's own name even when changed
+ * main - to return name of file
  * @argc: argument to count argument
  * @argv: output content of arguments
  * Return: 0
  */
 
-int main(int argc __attribute((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
-
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
 	return (0);
 }
