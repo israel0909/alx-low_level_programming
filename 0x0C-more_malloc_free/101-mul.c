b22e87a9a68ea95dec933c502b29d6fb49375570
x9x96 <israelsbook@gmail.com>
2023-03-16 14:32:58 -0700

    test #101

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,102 @@
+#include "main.h"
+
+int is_digit(char *s);
+int slen(char *s);
+void err(void);
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: argument count
+ * @argv: argument content
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		err();
+	len1 = slen(s1);
+	len2 = slen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
+	{
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = slen(s2) - 1; len2 >= 0; len2--)
+		{
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
+	}
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
+	return (0);
+}
+
+/**
+ * is_digit - checks if  string has  a non-digit char
+ * @s: string
+ * Return: 1 (success)
+ */
+
+int is_digit(char *s)
+{
+	int x = 0;
+
+	while (s[x])
+	{
+		if (s[x] < '0' || s[x] > '9')
+			return (0);
+		x++;
+	}
+	return (1);
+}
+
+/**
+ * slen - returns length of string
+ * @s: string
+ * Return: length of string
+ */
+
+int slen(char *s)
+{
+	int x = 0;
+
+	while (s[x] != '\0')
+	{
+		x++;
+	}
+	return (x);
+}
+
+/**
+ * err - checks for errors
+ */
+
+void err(void)
+{
+	printf("Error\n");
+	exit(98);
+}
