e7368911c379f832dada1fb75c3ff858a0bce2cd
x9x96 <israelsbook@gmail.com>
2023-03-22 12:30:00 -0700

    test #1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - performs function on  array elements
+ * @array:array
+ * @size: array size
+ * @action: function pointer
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array == NULL || action == NULL)
+		return;
+
+	while (0 < size--)
+	{
+		action(*array);
+		array++;
+	}
+}
