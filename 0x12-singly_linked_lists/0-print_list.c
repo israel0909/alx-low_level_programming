ba0c36f49c9d8b78d3b0a21030b34a46f8aef0b7
x9x96 <israelsbook@gmail.com>
2023-03-30 14:19:49 -0700

    fixing #0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
  * print_list - prints all elements of a linked list
@@ -8,8 +9,6 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t x = 0;
-
 	size_t cnt = 0;
 
 	while (h != NULL)
