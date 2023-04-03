a09d5618d9d2444ecb236daaa27413baa8f691ca
x9x96 <israelsbook@gmail.com>
2023-04-03 15:13:56 -0700

    test #5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees listint_t list
+ * @head: listint_t pointer
+ * Return: nil
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp_node;
+
+	while (*head)
+	{
+		temp_node = (*head)->next;
+		free(*head);
+		*head = temp_node;
+	}
+
+	*head = NULL;
+	
+	if (head == NULL)
+	{
+		return;
+	}
+}
