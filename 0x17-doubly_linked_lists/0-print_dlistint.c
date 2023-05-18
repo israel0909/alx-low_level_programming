56c909cd2ce52b39a01b453de344080038ec2c6f
x9x96 <israelsbook@gmail.com>
2023-05-18 10:03:12 -0700

    betty fix a

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,13 +1,11 @@
 #include "lists.h"
 
-/*
+/**
  * print_dlistint - prints all data in a list
  * @h: list's head pointer
  *
  * Return: 1(success), -1 (err0r)
- *
  */
-
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t nodes = 0;
